cf276de92b6aeab706555c4fcdb9a9145c6cd264
KobeBryant24-rgb <60447822+KobeBryant24-rgb@users.noreply.github.com>
2020-02-21 13:16:08 -0800

    Update Program.cs

--- Setup/Program.cs
+++ Setup/Program.cs
@@ -1,1 +1,47 @@
+using System;
 
+namespace Peliculas
+{
+    class pelicula
+    {
+        private string titulo;
+        private Int16 año;
+        private string pais;
+        private string director;
+
+        public pelicula(string titulo, Int16 año)
+        {
+            this.titulo=titulo;
+            this.año=año;
+        }
+        public void imprime()
+        {
+            Console.WriteLine("{0} {1}  ",titulo,año);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pelicula p1 = new pelicula("Gravity",2013);
+            p1.imprime();
+
+            pelicula p2 = new pelicula("Joker",2019);
+            p2.imprime();
+            
+            pelicula p3 = new pelicula("Toy Story 4",2019);
+            p3.imprime();
+
+            pelicula p4 = new pelicula("Bohemian Rapsody",2018);
+            p4.imprime();
+            
+            pelicula p5 = new pelicula("Roma",2018);
+            p5.imprime();
+            
+            
+                
+        }
+            
+    }
+}
