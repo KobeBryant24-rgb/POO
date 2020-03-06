af06b07af3bcf7c55c269ef80fa2b244dea735cd
KobeBryant24-rgb <60447822+KobeBryant24-rgb@users.noreply.github.com>
2020-03-06 12:32:26 -0800

    Title

---
+++ Lista_Peliculas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lista_Peliculas
+{
+    class Lista_Peliculas{
+        public string nombre;
+        public void imprime(){
+            Console.WriteLine(nombre);
+        }
+        public Lista_Peliculas(string n){
+        nombre = n;
+    }
+    
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+           List <Lista_Peliculas> Lista_Peliculas = new List<Lista_Peliculas>();
+           Lista_Peliculas.Add(new Lista_Peliculas("Termineitor"));
+           Lista_Peliculas.Add(new Lista_Peliculas("Jeilo"));
+           Lista_Peliculas.Add(new Lista_Peliculas("Col of dury"));
+           Lista_Peliculas.Add(new Lista_Peliculas("Fornais"));
+           Lista_Peliculas.Add(new Lista_Peliculas("Girs of guars"));
+           for(int i = 0; i<Lista_Peliculas.Count; i++){
+               Lista_Peliculas[i].imprime();
+           }
+        }
+    }
+}
