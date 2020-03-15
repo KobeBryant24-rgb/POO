88e197bdc599ed10b0ebe9f0622a5b2bba2f1beb
KobeBryant24-rgb <60447822+KobeBryant24-rgb@users.noreply.github.com>
2020-03-14 18:19:51 -0700

    Update Program.cs

--- Lista_Actores/Program.cs
+++ Lista_Actores/Program.cs
@@ -1,58 +1,157 @@
-﻿uusing System;
+﻿using System;
 
 using System.Collections.Generic;
 
-namespace Lista_Peliculas
+
+
+namespace Lista_Actores
 
 {
 
-    class Lista_Peliculas{
+    class Lista_Actores
 
-        public string nombre;
+    {
 
-        public void imprime(){
+        private string act;
 
-            Console.WriteLine(nombre);
+        private Int16 añoa;
+
+        public Lista_Actores(string a, Int16 p)
+
+        {
+
+            act = a;
+
+            año = p;
 
         }
-         public string año;
 
-        public void imprime(){
+        public string getActor()
 
-            Console.WriteLine(año);
+        {
+
+            return act;
 
         }
 
+        public Int16 getAñoa()
 
+        {
 
-        public Lista_Peliculas(string n, int q){
+            return año;
 
-        nombre = n;
-        año = q;
+        }
 
     }
 
-    
+    class Peliculas
+
+    {
+
+       private string nombre;
+
+       private Int16 año;
+
+       private List<Lista_Actores> actores;
+
+       public Peliculas(string nombre, Int16 año)
+
+       {
+
+           this.nombre=nombre;
+
+           this.año=año;
+
+           actores = new List <Lista_Actores>();
+
+       }
+
+
+
+       public string getPelicula()
+
+       {
+
+           return nombre;
+
+       }
+
+       public int getAñoP()
+
+       {
+
+           return año;
+
+       }
+
+       public void AgregaActor(actor a)
+
+       {
+
+           actores.Add(a);
+
+       }
+
+       public void ImprimeActores()
+
+       {
+
+           Console.WriteLine("La pelicula " + "{0}" + " fue estrenada en el año " + "{1}", nombre,año);
+
+           foreach (actor a in actores){
+
+               Console.WriteLine("El papel principal fue interpretado por " + "{0}" + " nacido(a) en el año " + "{1}", a.getActor(), a.getAñoa());
+
+           }
+
+           Console.WriteLine();
+
+       }
 
     }
-
-
-
 
     class Program
-    {
-        static void Main(string[] args)
-        {
-           
 
-           List <Lista_Actores> Lista_Actores = new List<Lista_Actores>();
-           Lista_Actores.Add(new Lista_Actores("Emma ", 16));
-           Lista_Actores.Add(new Lista_Actores("Stone", 20 ));
-           Lista_Actores.Add(new Lista_Actores("Col of dury",21));
-           Lista_Actores.Add(new Lista_Actores("Fornais", 2012));
-           Lista_Actores.Add(new Lista_Actores("Girs of guars"));
-           foreach(Lista_Actores a in Lista_Actores)
-           a.imprime();
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Peliculas p1 = new Peliculas ("Avengers", 2012);
+
+            p1.AgregaActor(new actor ("Robert Dawney JR", 1965));
+
+            p1.ImprimeActores();
+
+            Peliculas p2 = new Peliculas ("Joker", 2019);
+
+            p2.AgregaActor(new actor ("Joaquin Phoenix", 1974));
+
+            p2.ImprimeActores();
+
+            Peliculas p3 = new Peliculas ("Rapido y Furioso", 2009);
+
+            p3.AgregaActor(new actor ("Vin Diesel", 1967));
+
+            p3.ImprimeActores();
+
+            Peliculas p4 = new Peliculas ("Jurassic World", 2015);
+
+            p4.AgregaActor(new actor ("Chris Pratt", 1979));
+
+            p4.ImprimeActores();
+
+            Peliculas p5 = new Peliculas ("King Kong", 2005);
+
+            p5.AgregaActor(new actor ("Naomi Watts", 1968));
+
+            p5.ImprimeActores();
+
+
+
         }
+
     }
- }
+
+}
